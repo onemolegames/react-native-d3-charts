dbe0c681cb9ba6ecf1f356c990c4d1ebe52e992d
semirturgay <semir.turgay@gmail.com>
2017-11-21 21:49:31 +0300

    - Line chart improvements - adding axises to line chart

--- src/line/Line.js
+++ src/line/Line.js
@@ -1,40 +1,37 @@
 import React from "react";
-import Svg, {G, Path, Text, Circle} from "react-native-svg";
-import PropTypes from "prop-types";
+import Svg, {G, Path, Use, Line, Text} from "react-native-svg";
 import * as scale from "d3-scale";
 import * as shape from "d3-shape";
+import * as array from "d3-array";
+import PropTypes from "prop-types";
 
 const d3 = {
   scale,
-  shape
+  shape,
+  array
 };
-const {number, string, arrayOf} = PropTypes;
+const {number, string, arrayOf, instanceOf} = PropTypes;
 const object = PropTypes.shape;
 
+
 const LineChart = (props) => {
+  const {size, series, xAxis, yAxis, margin} = props;
 
-  const {size, series, marginBottom, marginTop, marginRight, marginLeft} = props;
-  let maxYvalue = Math.max(...[].concat.apply([], series.map(serie => serie.data)).map(d => d.y));
-  let maxXvalue = Math.max(...[].concat.apply([], series.map(serie => serie.data)).map(d => d.x));
-  const y = scale.scaleLinear().domain([0, maxYvalue])
-    .range([marginBottom, size.height - ( marginTop)]);
+  const y = scale.scaleLinear().domain([0, array.max(series[0].data)])
+    .range([margin, size.height - margin]);
 
-  const x = scale.scaleLinear().domain([0, maxXvalue])
-    .range([marginLeft, size.width - ( marginRight)]);
+  const x = scale.scaleLinear().domain([0, series[0].data.length])
+    .range([margin, size.width - margin]);
 
   const line = d3.shape.line()
     .x(function (d, i) {
-      return x(d.x);
+      return x(i);
     })
     .y(function (d) {
-      return -1 * y(d.y);
+      return -1 * y(d);
     }).curve(d3.shape.curveNatural);
 
   const lines = series.map((serie, index) => {
-    let data = serie.data;
-    if (index > 0) {
-      data = [series[index - 1].data[series[index - 1].data.length - 1], ...serie.data]
-    }
     return (
       <Path
         key={index}
@@ -42,33 +39,41 @@ const LineChart = (props) => {
         strokeWidth={serie.width}
         strokeOpacity={serie.opacity}
         stroke={serie.color}
-        d={line(data)}/>
+        d={line(serie.data)}/>
     );
   });
 
-  const circles = series.map((serie, index) => {
-    return serie.data.map((s, i) => {
-      return (
-        <G key={index + i}>
-          <Circle cx={x(s.x)} cy={-1*y(s.y)} r={serie.circleWidth} fill={serie.circleColor}
-                  stroke={serie.circleColor}/>
-          <Text x={x(s.x)-15} y={-1*y(s.y)-30}
-                fontFamily={serie.fontFamily}
-                fontWeight={"100"}
-                fill={serie.circleFontColor}
-                fontSize={serie.circleFontSize}
-                stroke={serie.circleFontColor}>{s.y}</Text>
-        </G>
-      );
-    });
+  const yLabels = y.ticks(yAxis.ticks).map((tick, index) => {
+    return (
+      <G key={index}>
+        <Text x={-10} y={-1 * y(tick)} dy="-5">{tick}</Text>
+        <Line x1={x(-0.05)} x2={0} y1={-1 * y(tick)} y2={-1 * y(tick)} stroke="red" strokeWidth="2"/>
+      </G>
+    );
+  });
+
+  const xLabels = x.ticks(xAxis.ticks).map((tick, index) => {
+    return (
+      <G key={index}>
+        <Text x={-10} y={-1 * x(tick)} dy="-5">{tick}</Text>
+        <Line x1={x(-0.05)} x2={-1*x(tick)} y1={0} y2={0} stroke="red" strokeWidth="2"/>
+      </G>
+    );
   });
 
   return (
-    <Svg width={size.width} height={size.height} fill="none" style={{borderWidth:2}}>
+    <Svg width={size.width} height={size.height} fill="none">
       <G x="10" y={size.height}>
         {lines}
-        {circles}
+        <Line x1={x(0)} x2={x(size.width)} y1={-1 * y(0)} y2={-1 * y(0)} stroke="red"
+              strokeWidth="2"/>
+        <Line x1={x(0)} x2={x(0)} y1={-1*y(0)} y2={-1 * y(array.max(series[0].data))}
+              stroke="red"
+              strokeWidth="2"/>
+        {yLabels}
+
       </G>
+      <Use href={"#10"}/>
     </Svg>
   );
 };
@@ -78,12 +83,18 @@ LineChart.propTypes = {
     width: number.isRequired,
     height: number.isRequired
   }).isRequired,
-  marginRight: number,
-  marginLeft: number,
-  marginTop: number,
-  marginBottom: number,
+  xAxis: object({
+    type: string.isRequired,
+    start: instanceOf(Date),
+    end: instanceOf(Date),
+    ticks: number
+  }),
+  margin: number,
+  yAxis: object({
+    ticks: number
+  }),
   series: arrayOf(object({
-    data: arrayOf(object({x: number, y: number})),
+    data: arrayOf(number),
     color: string,
     name: string,
     width: number,
@@ -96,38 +107,32 @@ LineChart.defaultProps = {
     width: 300,
     height: 150
   },
-  marginRight: 15,
-  marginLeft: 15,
-  marginTop: 35,
-  marginBottom: 0,
+  xAxis: {
+    type: "Date",
+    start: new Date(),
+    end: new Date("October 13, 2018 11:47:00"),
+    ticks: 5
+  },
+  yAxis: {
+    ticks: 5
+  },
+  margin: 10,
   series: [
     {
-      data: [{x: 0, y: 28},
-        {x: 2, y: 27},
-        {x: 4, y: 25}],
-      color: "#4AF55D",
-      name: "Serie 1",
-      circleColor: "#4AF55D",
-      circleWidth: 5,
-      circleFontColor: "#4AF55D",
-      circleFontSize: 15,
-      fontFamily: "",
+      data: [4, 7, 0, 4, 7, 0, 4, 7, 0, 4, 8, 4, 7],
+      color: "#620b79",
+      name: "Serie 0",
+      width: 2,
+      opacity: 0.4
+    },
+    {
+      data: [0, 6, 8, 2, 3, 5, 4, 7, 0, 4, 7, 4, 7],
+      color: "#791a22",
+      name: "Serie 0",
       width: 2,
       opacity: 1
     },
-    {
-      data: [{x: 6, y: 21}, {x: 8, y: 20}],
-      color: "#ffc416",
-      name: "Serie 2",
-      circleColor: "#ffc416",
-      circleWidth: 5,
-      circleFontColor: "#ffc416",
-      circleFontSize: 15,
-      fontFamily: "",
-      width: 2,
-      opacity: 1
-    }]
-
+  ]
 };
 
 export default LineChart;
